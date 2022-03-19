ec79658eb572ad82c6ee4f55b5c14d867480c29d
h-yoshikai <exbass11@gmail.com>
2022-03-20 00:52:46 +0900

    ✨ useEffectの処理を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,22 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useCallback } from "react/cjs/react.development";
+import { useEffect } from "react";
 import { Footer, Header, Main } from "src/components";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    alert(123);
+  // const handleClick = useCallback((e) => {
+  //   e.preventDefault();
+  //   alert(123);
+  // }, []);
+  useEffect(() => {
+    // マウント時
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      // アンマウント時
+      document.body.style.backgroundColor = "";
+    };
   }, []);
   return (
     <div className={styles.container}>
@@ -15,9 +24,9 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <button href="/about" onClick={handleClick}>
         ボタン
-      </a>
+      </button> */}
       <Main page="index" />
 
       <Footer />
