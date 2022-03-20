9c4efc6b1949222eea7450325f1926fed92c23e2
h-yoshikai <exbass11@gmail.com>
2022-03-20 23:17:58 +0900

    ⚡️ useCallbakとuseEffectの第2引数を変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,28 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { Footer, Header, Main } from "src/components";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    // 関数にすると、呼ばれるタイミングで前回の状態を正しく反映することができる
-    setCount((count) => count + 1);
-    // setFoo(foo + 1);
-    // setFoo(foo + 1);
-    // これは、
-    // setFoo(1 + 1);
-    // setFoo(1 + 1);
-    // と同じだから2プラスされない
-  };
+  const handleClick = useCallback(
+    () => {
+      // 関数にすると、呼ばれるタイミングで前回の状態を正しく反映することができる
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+      // setFoo(foo + 1);
+      // setFoo(foo + 1);
+      // これは、
+      // setFoo(1 + 1);
+      // setFoo(1 + 1);
+      // と同じだから2プラスされない
+    },
+    [count] //ここが変化するたびに再生成
+  );
+  // 空配列だと関数が再生成されないので、関数内の変数がずっと同じ
+  // 関数で書くと正しく前回の値が使用される
 
   useEffect(() => {
     // マウント時
@@ -25,7 +32,8 @@ export default function Home() {
       // アンマウント時
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
+
   return (
     <div className={styles.container}>
       <Head>
