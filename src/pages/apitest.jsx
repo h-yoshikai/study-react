92ceaf6bb89187ca5ed2d46903dabf5b7cf70342
h-yoshikai <exbass11@gmail.com>
2022-03-22 00:09:11 +0900

    🚸 ローディング状態とエラー状態の追加

--- src/pages/apitest.jsx
+++ src/pages/apitest.jsx
@@ -1,34 +1,17 @@
 import Head from "next/head";
 import { useEffect, useState, useCallback } from "react";
 import { Footer, Header, Main } from "src/components";
+import { Posts } from "src/components/Posts";
 import styles from "src/styles/Home.module.css";
 
 const APITest = () => {
-  const [posts, setPosts] = useState([]);
-
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
