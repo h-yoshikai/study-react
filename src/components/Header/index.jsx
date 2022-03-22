060742d151b25de6289806b69ee79c251f243417
h-yoshikai <exbass11@gmail.com>
2022-03-22 21:37:31 +0900

    🚚 投稿系をpostsに変更

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import classes from "./Header.module.css";
 const NAV_ITEMS = [
   { href: "/", label: "Index" },
   { href: "/about", label: "About" },
-  { href: "/apitest", label: "APITest" },
+  { href: "/posts", label: "Posts" },
 ];
 
 export const Header = () => {
