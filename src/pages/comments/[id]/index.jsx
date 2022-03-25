fe0eabbf3dd1ad925140c586234f3a373e4badb9
h-yoshikai <exbass11@gmail.com>
2022-03-25 23:46:38 +0900

    ⚡️ コメント詳細ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,11 +1,44 @@
 import { Header } from "src/components";
 import { Comment } from "src/components/Comment";
+import { SWRConfig } from "swr";
 
-const CommentsId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: {
+      id: comment.id.toString(),
+    },
+  }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentData,
+      },
+    },
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
   return (
     <div>
-      <Header />
-      <Comment />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
