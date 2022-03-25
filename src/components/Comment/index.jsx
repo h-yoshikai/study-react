f5156e61617baa51b71ffd085dc9611bc253b556
h-yoshikai <exbass11@gmail.com>
2022-03-25 15:09:26 +0900

    ✨ コメントページで元の投稿を表示

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { PostByCommentId } from "src/components/Post/PostByCommentId";
 import { useComment } from "src/hooks/useComment";
 
 export const Comment = () => {
@@ -24,6 +25,8 @@ export const Comment = () => {
             <li>{data.name}</li>
             <li>{data.email}</li>
           </ul>
+          <h2>元の記事</h2>
+          <PostByCommentId id={data.postId} />
         </div>
       ) : null}
     </div>
---
+++ src/components/Post/PostByCommentId.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Link from "next/link";
+import { usePost } from "src/hooks/usePost";
+
+export const PostByCommentId = (props) => {
+  const { data, error, isLoading } = usePost(props.id);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{data?.title}</title>
+      </Head>
+      <Link href={`/posts/${data.id}`}>
+        <a>{data?.title}</a>
+      </Link>
+    </div>
+  );
+};
